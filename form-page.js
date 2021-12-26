d2f1d61ba1834d4b888c713999688cf27c1c6d7c
ToiYioT <toiyiot@gmail.com>
2021-12-26 18:26:34 +0200

    form-page: add "other roots" and "all forms" sections

--- form-page.js
+++ form-page.js
@@ -6,6 +6,7 @@ import { util } from "./scripts/util.js";
 const pronouns = pronounsConst;
 
 window.addEventListener("load", () => {
+    populateAllForms();
     getConjugations();
 })
 
@@ -13,27 +14,55 @@ window.addEventListener("load", () => {
 function getConjugations() {
     const form = getFilteringParams().forms[0];
 
-    const allLessons = ["", 1, 2, 3, 4, 5, 6, 7, 8];
     const formTitle = document.getElementsByClassName("form-title")[0];
     const tableRows = document.getElementsByClassName("table-row");
 
     const rootForm = forms[form];
     const rootArabic = rootForm.representativeRoot;
-    const allFormRoots = filterRoots({ forms: [form], lessons: allLessons });
 
-    conjugator.rootProcessing(rootArabic, rootForm, rootForm);
-    formTitle.innerHTML = conjugate(pronouns[4]);
+    populateAllRootsOfForm(rootForm);
+
+    formTitle.innerHTML += conjugate(rootArabic, rootForm, pronouns[4]);
 
     for (let i = 0; i < tableRows.length; i++) {
         const pronoun = pronouns[i];
-        tableRows[i].innerHTML += " - " + pronoun.arabic + " " + conjugate(pronoun);
+        tableRows[i].innerHTML += " - " + pronoun.arabic + " "
+            + conjugate(rootArabic, rootForm, pronoun);
     }
 
-    function conjugate(pronoun) {
-        let output = conjugator.getWord(rootForm[pronoun.name].template);
-        output = util.substituteLetterAtEndToEndingLetter(output, true);
-        output = util.postProcess(output);
-        return output;
+}
+
+function conjugate(rootArabic, conjugationForm, pronoun) {
+    conjugator.rootProcessing(rootArabic, conjugationForm, conjugationForm);
+    let output = conjugator.getWord(conjugationForm[pronoun.name].template);
+    output = util.substituteLetterAtEndToEndingLetter(output, true);
+    output = util.postProcess(output);
+    return output;
+}
+
+function populateAllRootsOfForm(form) {
+
+    const allLessons = ["", 1, 2, 3, 4, 5, 6, 7, 8];
+    const allFormRoots = filterRoots({ forms: [form.formName], lessons: allLessons });
+    const otherRootsContainer = document.getElementsByClassName("other-roots")[0];
+
+    for (let i = 0; i < allFormRoots.length; i++) {
+        const root = allFormRoots[i];
+        const conjugated = conjugate(root.arabic, form, pronouns[4]);
+        otherRootsContainer.textContent += conjugated + " - " + root.hebrew + ", ";
+    }
+}
+
+function populateAllForms() {
+
+    const allFormsContainer = document.getElementsByClassName("all-forms")[0];
+
+    for (const [formName, form] of Object.entries(forms)) {
+
+        const representativeRoot = form.representativeRoot;
+        conjugator.rootProcessing(representativeRoot, form, form);
+        const conjugated = conjugate(representativeRoot, form, pronouns[4]);
+        allFormsContainer.textContent += conjugated + ", ";
     }
 }
 
