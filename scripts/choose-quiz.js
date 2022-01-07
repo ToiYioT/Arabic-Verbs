2b5084737a06d43ecad68ce0dbb31e424e257273
ToiYioT <toiyiot@gmail.com>
2022-01-07 09:46:42 +0200

    separate the quiz logic to another file

---
+++ scripts/choose-quiz.js
@@ -0,0 +1,227 @@
+import { AnswerButton } from "./views.js";
+import { util } from "./util.js";
+import { conjugator } from "./conjugator.js";
+import { forms, tenses } from "./tenses.js";
+import { getConfuseType } from "./urlParams.js";
+
+const numOfAnswers = 4;
+
+let answerButtons = [];
+let getAnswersFunction;
+let correctAnswer;
+
+let tense;
+let questionHolder;
+let questionDispenser;
+
+let playSound;
+let updateScore;
+let mainButton;
+
+function init(qDispenser, button, playSoundFunction, updateScoreFunction) {
+    questionDispenser = qDispenser;
+    playSound = playSoundFunction;
+    updateScore = updateScoreFunction;
+    mainButton = button;
+
+    questionHolder = document.getElementById("question");
+    setConfuseType();
+
+    for (let i = 0; i < numOfAnswers; i++) {
+        answerButtons.push(new AnswerButton(i + 1, answerClickHandler));
+    }
+}
+
+function initQuestionFromParams(qParams) {
+
+    hideAllIcons();
+    questionAnswered = false;
+    setTense(qParams);
+    // first one is the correct root form:
+    let answerForms = createAnswerForms(qParams);
+    let answers = getAnswersFunction(qParams.root.arabic, answerForms, qParams.pronoun);
+
+    correctAnswer = answers[0];
+    util.shuffle(answers);
+    setAnswersToButtons(answers);
+}
+
+function setTense(qParams) {
+    const form = forms[qParams.root.form];
+    tense = tenses[form.tense];
+}
+
+var questionAnswered = false;
+function answerClickHandler(clickedButton) {
+
+    if (questionAnswered) return;
+    questionAnswered = true;
+
+    for (let i = 0; i < answerButtons.length; i++) {
+
+        if (answerButtons[i].answer == correctAnswer) {
+            answerButtons[i].setCorrect();
+
+            if (clickedButton == answerButtons[i]) {
+                questionDispenser.registerCorrectAnswer(
+                    questionHolder.innerHTML,
+                    correctAnswer,
+                    answerButtons[i].answer);
+                playSound("correct");
+            }
+        } else {
+
+            if (clickedButton == answerButtons[i]) {
+                answerButtons[i].setIncorrect();
+                questionDispenser.registerIncorrectAnswer(
+                    questionHolder.innerHTML,
+                    correctAnswer,
+                    answerButtons[i].answer);
+                playSound("incorrect");
+
+            } else {
+                answerButtons[i].setFaded();
+            }
+        }
+    }
+
+    if (questionDispenser.isQuizMode() && questionDispenser.isLastQuestion()) {
+        mainButton.setEndQuiz();
+
+    } else {
+        mainButton.setNextQuestion();
+    }
+    updateScore();
+}
+
+function createAnswerForms(qParams) {
+
+    let form = forms[qParams.root.form];
+    let pronoun = qParams.pronoun;
+
+    let answerForms = [];
+
+    answerForms.push(form);
+    answerForms.push(forms[form[pronoun.name].distractingForms[0]]);
+    answerForms.push(forms[form[pronoun.name].distractingForms[1]]);
+    answerForms.push(forms[form[pronoun.name].distractingForms[2]]);
+
+    return answerForms;
+}
+
+function setConfuseType() {
+    let confuseType = getConfuseType();
+
+    switch (confuseType) {
+        case "form":
+            getAnswersFunction = getAnswersConfuseWithForms;
+            break;
+        case "pronoun":
+            getAnswersFunction = getAnswersConfuseWithPronouns;
+            break;
+        case "mixed":
+            getAnswersFunction = getAnswersAlternateConfuse;
+            break;
+        default:
+            getAnswersFunction = getAnswersConfuseWithForms;
+    }
+}
+
+function getAnswersConfuseWithForms(rootArabic, answerForms, pronoun) {
+    let answers = [];
+    let rootForm = answerForms[0];
+
+    for (let i = 0; i < numOfAnswers; i++) {
+        let conjugateTo = answerForms[i];
+        conjugator.rootProcessing(rootArabic, rootForm, conjugateTo);
+
+        let answer = pronoun.arabic + tense.answerPrefix
+            + conjugator.getWord(conjugateTo[pronoun.name].template);
+
+        answer = util.substituteLetterAtEndToEndingLetter(answer, true);
+        answer = util.postProcess(answer);
+        answers.push(answer);
+    }
+
+    if (tense.confuseWithPronouns && pronoun.confusingPronoun != null) {
+        answers = substituteLastAnswerWithConfusingPronoun(
+            answers, pronoun, rootArabic, rootForm);
+    }
+    return answers;
+}
+
+function getAnswersAlternateConfuse(rootArabic, answerForms, correctPronoun) {
+    if (Math.random() > 0.5) {
+        return getAnswersConfuseWithForms(rootArabic, answerForms, correctPronoun);
+    } else {
+        return getAnswersConfuseWithPronouns(rootArabic, answerForms, correctPronoun);
+    }
+}
+
+function getAnswersConfuseWithPronouns(rootArabic, answerForms, correctPronoun) {
+    let answers = [];
+    let correctAnswer;
+    let confusingPronounAnswer = null;
+    let confusingPronoun = correctPronoun.confusingPronoun;
+    let rootForm = answerForms[0];
+
+    pronouns = util.shuffle(pronouns);
+    conjugator.rootProcessing(rootArabic, rootForm, rootForm);
+
+    for (let i = 0; i < pronouns.length; i++) {
+
+        let answer = correctPronoun.arabic + tense.answerPrefix
+            + conjugator.getWord(rootForm[pronouns[i].name].template);
+
+        answer = util.substituteLetterAtEndToEndingLetter(answer, true);
+        answer = util.postProcess(answer);
+
+        answers.push(answer);
+        if (pronouns[i] == correctPronoun) {
+            correctAnswer = answer;
+        }
+        if (tense.confuseWithPronouns && pronouns[i] == confusingPronoun) {
+            confusingPronounAnswer = answer;
+        }
+    }
+    answers = [...new Set(answers)]; // leave only unique answers
+    answers = answers.filter(item => item !== correctAnswer); // remove correct ones
+    answers = answers.filter(item => item !== confusingPronounAnswer); // remove confusing pronoun
+    if (confusingPronounAnswer != null) {
+        answers.unshift(confusingPronounAnswer); // add back the confusingPronoun answer
+    }
+    answers.unshift(correctAnswer); // add back the correct one at the start of the array
+    answers.length = 4;
+    return answers;
+}
+
+function setAnswersToButtons(answers) {
+    for (let i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].setAnswer(answers[i]);
+        answerButtons[i].removeStyles();
+    }
+}
+
+function hideAllIcons() {
+    for (let i = 0; i < 4; i++) {
+        answerButtons[i].setNoIcons();
+    }
+}
+
+function substituteLastAnswerWithConfusingPronoun(
+    answers, pronoun, rootArabic, rootForm) {
+    let confusingPronoun = pronoun.confusingPronoun;
+    conjugator.rootProcessing(rootArabic, rootForm, rootForm);
+
+    let answer = pronoun.arabic + tense.answerPrefix
+        + conjugator.getWord(rootForm[confusingPronoun.name].template);
+    answer = util.substituteLetterAtEndToEndingLetter(answer, true);
+    answer = util.postProcess(answer);
+    answers[answers.length - 1] = answer;
+    return answers;
+}
+
+export const chooseQuiz = {
+    init,
+    initQuestionFromParams,
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 import { util } from "./util.js";
 import { debug } from "./debug.js";
-import { conjugator } from "./conjugator.js";
 import { pronounsConst, filterRoots } from "./data.js";
-import { forms, tenses } from "./tenses.js";
-import { getConfuseType, getFilteringParams } from "./urlParams.js";
-import { AnswerButton, Label, MainButton, Checkbox } from "./views.js";
+import { tenses, forms } from "./tenses.js";
+import { getFilteringParams } from "./urlParams.js";
+import { Label, MainButton, Checkbox } from "./views.js";
 import { questionDispenser } from "./question-dispenser.js"
+import { chooseQuiz } from "./choose-quiz.js";
 
 var tense;
 var roots;
@@ -13,20 +13,17 @@ var pronouns = pronounsConst;
 
 // settings
 var hideAnswers = true;
-const totalNumOfQuestions = 2;
-const numOfAnswers = 4;
 const progressBarUpdateInterval = 20;
 
 var numOfProgressBarUpdates = 0;
 const progressBarMaxUpdates = 150;
 
 // html elements
-var answerButtons = [];
-var answerSection;
 var questionHolder;
-var button;
+var mainButton;
 var score;
 var progressBar;
+let answerSection;
 
 var quizSummarySection;
 var summaryTitle, summaryScore, summaryMistakes;
@@ -35,20 +32,21 @@ var settingsWindow;
 var checkboxTimer;
 
 // logic variables 
-var correctAnswer;
 var progressBarTimer;
-var getAnswersFunction;
 
 // audio
 const soundCorrect = new Audio('./sounds/correct.mp3');
 const soundIncorrect = new Audio('./sounds/incorrect.mp3');
 const soundFinish = new Audio('./sounds/finish.mp3');
 
+
+// new restructuring:
+let currentQuestionEngine = chooseQuiz;
+
 window.addEventListener('load', (event) => {
 
     let filteringParams = getFilteringParams();
     roots = filterRoots(filteringParams);
-    setConfuseType();
 
     questionHolder = document.getElementById("question");
     quizSummarySection = document.getElementById("quiz-summary-section");
@@ -56,9 +54,11 @@ window.addEventListener('load', (event) => {
     summaryScore = document.getElementById("summary-score");
     summaryMistakes = document.getElementById("summary-mistakes");
 
-    button = new MainButton("main-button", buttonHandler, showSummaryScreen);
+    mainButton = new MainButton("main-button", buttonHandler, showSummaryScreen);
     score = new Label("score");
     progressBar = document.getElementById("progress-bar");
+
+    chooseQuiz.init(questionDispenser, mainButton, playSound, updateScore);
 
     answerSection = document.getElementById("answer-section");
 
@@ -67,55 +67,28 @@ window.addEventListener('load', (event) => {
     document.getElementsByClassName("x-button")[0].onclick = closeSettingsWindow;
     checkboxTimer = new Checkbox("checkbox-timer");
 
-
-    for (let i = 0; i < numOfAnswers; i++) {
-        answerButtons.push(new AnswerButton(i + 1, answerClickHandler));
-    }
-
     resetGame();
     // debug.showConjugations(roots, pronouns, conjugator, forms);
     // debug.showConjugationHebrew(roots, pronouns, tenses, forms);
 });
 
-function setConfuseType() {
-    let confuseType = getConfuseType();
 
-    switch (confuseType) {
-        case "form":
-            getAnswersFunction = getAnswersConfuseWithForms;
-            break;
-        case "pronoun":
-            getAnswersFunction = getAnswersConfuseWithPronouns;
-            break;
-        case "mixed":
-            getAnswersFunction = getAnswersAlternateConfuse;
-            break;
-        default:
-            getAnswersFunction = getAnswersConfuseWithForms;
-    }
-}
-
-function setTense(form) {
-    tense = tenses[form.tense];
-}
 
 function buttonHandler() {
 
     if (!hideAnswers) {
         initQuestion();
-        button.setChooseAnswer();
-        hideAllIcons();
+        mainButton.setChooseAnswer();
 
     } else {
-        if (button.state == "show-answers") {
+        if (mainButton.state == "show-answers") {
             cancelTimerAndShowQuestion();
 
-        } else if (button.state == "next-question") {
-            hideAllIcons();
+        } else if (mainButton.state == "next-question") {
             answerSection.classList.add("hide");
 
             startProgressBarAnimation();
-            button.setShowAnswers();
+            mainButton.setShowAnswers();
             initQuestion();
         }
     }
@@ -132,7 +105,7 @@ function showSummaryScreen() {
     score.setText("");
     progressBar.style.width = "100%";
     answerSection.classList.add("hide");
-    button.setRestartQuiz(resetGame);
+    mainButton.setRestartQuiz(resetGame);
 
     quizSummarySection.style.display = "flex";
     questionHolder.style.display = "none";
@@ -150,110 +123,30 @@ function initQuestion() {
     soundFinish.src = "./sounds/finish.mp3";
     soundIncorrect.src = "./sounds/incorrect.mp3";
     soundCorrect.src = "./sounds/correct.mp3";
-
-    questionDispenser.handleNewQuestion();
-    let currentQuestion = questionDispenser.getCurrentQuestion();
-
-    initQuestionFromParams(currentQuestion);
-}
-
-function initQuestionFromParams(qParams) {
-    questionAnswered = false;
     updateScore();
 
-    let pronoun = qParams.pronoun;
-    // first one is the correct root form:
-    let answerForms = createAnswerForms(qParams);
-    setTense(answerForms[0]);
+    questionDispenser.handleNewQuestion();
+    const qParams = questionDispenser.getCurrentQuestion();
+    setTense(qParams);
+    conjugateHebrew(qParams.root.hebrew, qParams.pronoun);
 
-    conjugateHebrew(qParams.root.hebrew, pronoun);
-    let answers = getAnswersFunction(qParams.root.arabic, answerForms, pronoun);
-
-    correctAnswer = answers[0];
-    util.shuffle(answers);
-    setAnswersToButtons(answers);
+    currentQuestionEngine.initQuestionFromParams(qParams);
 }
 
-function getAnswersConfuseWithForms(rootArabic, answerForms, pronoun) {
-    let answers = [];
-    let rootForm = answerForms[0];
+function playSound(state) {
+    if (state == "correct") {
+        soundCorrect.play();
 
-    for (let i = 0; i < numOfAnswers; i++) {
-        let conjugateTo = answerForms[i];
-        conjugator.rootProcessing(rootArabic, rootForm, conjugateTo);
-
-        let answer = pronoun.arabic + tense.answerPrefix
-            + conjugator.getWord(conjugateTo[pronoun.name].template);
-
-        answer = util.substituteLetterAtEndToEndingLetter(answer, true);
-        answer = util.postProcess(answer);
-        answers.push(answer);
-    }
-
-    if (tense.confuseWithPronouns && pronoun.confusingPronoun != null) {
-        answers = substituteLastAnswerWithConfusingPronoun(
-            answers, pronoun, rootArabic, rootForm);
-    }
-    return answers;
-}
-
-function getAnswersAlternateConfuse(rootArabic, answerForms, correctPronoun) {
-    if (Math.random() > 0.5) {
-        return getAnswersConfuseWithForms(rootArabic, answerForms, correctPronoun);
-    } else {
-        return getAnswersConfuseWithPronouns(rootArabic, answerForms, correctPronoun);
+    } else if (state = "incorrect") {
+        soundIncorrect.play();
     }
 }
 
-function getAnswersConfuseWithPronouns(rootArabic, answerForms, correctPronoun) {
-    let answers = [];
-    let correctAnswer;
-    let confusingPronounAnswer = null;
-    let confusingPronoun = correctPronoun.confusingPronoun;
-    let rootForm = answerForms[0];
-
-    pronouns = util.shuffle(pronouns);
-    conjugator.rootProcessing(rootArabic, rootForm, rootForm);
-
-    for (let i = 0; i < pronouns.length; i++) {
-
-        let answer = correctPronoun.arabic + tense.answerPrefix
-            + conjugator.getWord(rootForm[pronouns[i].name].template);
-
-        answer = util.substituteLetterAtEndToEndingLetter(answer, true);
-        answer = util.postProcess(answer);
-
-        answers.push(answer);
-        if (pronouns[i] == correctPronoun) {
-            correctAnswer = answer;
-        }
-        if (tense.confuseWithPronouns && pronouns[i] == confusingPronoun) {
-            confusingPronounAnswer = answer;
-        }
-    }
-    answers = [...new Set(answers)]; // leave only unique answers
-    answers = answers.filter(item => item !== correctAnswer); // remove correct ones
-    answers = answers.filter(item => item !== confusingPronounAnswer); // remove confusing pronoun
-    if (confusingPronounAnswer != null) {
-        answers.unshift(confusingPronounAnswer); // add back the confusingPronoun answer
-    }
-    answers.unshift(correctAnswer); // add back the correct one at the start of the array
-    answers.length = 4;
-    return answers;
+function setTense(qParams) {
+    const form = forms[qParams.root.form];
+    tense = tenses[form.tense];
 }
 
-function substituteLastAnswerWithConfusingPronoun(
-    answers, pronoun, rootArabic, rootForm) {
-    let confusingPronoun = pronoun.confusingPronoun;
-    conjugator.rootProcessing(rootArabic, rootForm, rootForm);
-
-    let answer = pronoun.arabic + tense.answerPrefix
-        + conjugator.getWord(rootForm[confusingPronoun.name].template);
-    answer = util.substituteLetterAtEndToEndingLetter(answer, true);
-    answer = util.postProcess(answer);
-    answers[answers.length - 1] = answer;
-    return answers;
-}
 
 function conjugateHebrew(rootHebrew, pronoun) {
     // extracting the hebrew word to conjugate (the first one)
@@ -279,19 +172,6 @@ function conjugateHebrew(rootHebrew, pronoun) {
     }
 }
 
-function setAnswersToButtons(answers) {
-    for (let i = 0; i < answerButtons.length; i++) {
-        answerButtons[i].setAnswer(answers[i]);
-        answerButtons[i].removeStyles();
-    }
-}
-
-function hideAllIcons() {
-    for (let i = 0; i < 4; i++) {
-        answerButtons[i].setNoIcons();
-    }
-}
-
 function startProgressBarAnimation() {
 
     clearInterval(progressBarTimer);
@@ -314,52 +194,9 @@ function cancelTimerAndShowQuestion() {
     progressBar.style.width = "100%";
     progressBarTimer = false;
     answerSection.classList.remove("hide");
-    button.setChooseAnswer();
+    mainButton.setChooseAnswer();
 }
 
-var questionAnswered = false;
-function answerClickHandler(clickedButton) {
-
-    if (questionAnswered) return;
-    questionAnswered = true;
-
-    for (let i = 0; i < answerButtons.length; i++) {
-
-        if (answerButtons[i].answer == correctAnswer) {
-            answerButtons[i].setCorrect();
-
-            if (clickedButton == answerButtons[i]) {
-                questionDispenser.registerCorrectAnswer(
-                    questionHolder.innerHTML,
-                    correctAnswer,
-                    answerButtons[i].answer);
-                soundCorrect.play();
-            }
-        } else {
-
-            if (clickedButton == answerButtons[i]) {
-                answerButtons[i].setIncorrect();
-                questionDispenser.registerIncorrectAnswer(
-                    questionHolder.innerHTML,
-                    correctAnswer,
-                    answerButtons[i].answer);
-                soundIncorrect.play();
-
-            } else {
-                answerButtons[i].setFaded();
-            }
-        }
-    }
-
-    if (questionDispenser.isQuizMode() && questionDispenser.isLastQuestion()) {
-        button.setEndQuiz();
-
-    } else {
-        button.setNextQuestion();
-    }
-
-    updateScore();
-}
 
 function updateScore() {
 
@@ -367,24 +204,11 @@ function updateScore() {
     score.setText(titleText);
 }
 
-function createAnswerForms(qParams) {
-
-    let form = forms[qParams.root.form];
-    let pronoun = qParams.pronoun;
-
-    let answerForms = [];
-
-    answerForms.push(form);
-    answerForms.push(forms[form[pronoun.name].distractingForms[0]]);
-    answerForms.push(forms[form[pronoun.name].distractingForms[1]]);
-    answerForms.push(forms[form[pronoun.name].distractingForms[2]]);
-
-    return answerForms;
-}
 
 function openSettingsWindow() {
     settingsWindow.style.visibility = "visible";
 }
+
 function closeSettingsWindow() {
     settingsWindow.style.visibility = "hidden";
 
@@ -399,7 +223,7 @@ function closeSettingsWindow() {
 }
 
 function resetGame() {
-    questionDispenser.resetGame("quiz", roots, pronouns, totalNumOfQuestions);
+    questionDispenser.resetGame("quiz", roots, pronouns);
 
     quizSummarySection.style.display = "none";
     questionHolder.style.display = "flex";
@@ -408,13 +232,12 @@ function resetGame() {
     if (hideAnswers) {
         answerSection.classList.add("hide");
         startProgressBarAnimation();
-        button.setShowAnswers();
+        mainButton.setShowAnswers();
     } else {
         answerSection.classList.remove("hide");
-        button.setChooseAnswer();
+        mainButton.setChooseAnswer();
     }
 
-    hideAllIcons();
     initQuestion();
     updateScore();
 }
